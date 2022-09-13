567fec91727ec815095ab25352bf67d515f730db
jmininel <ju.mininel@hotmail.com>
2022-09-12 22:01:57 -0300

    create tailwind style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,19 @@
+import './styles/main.css';
 
-
+import logoImg from './assets/logo-nlw-esports.svg';
 
 function App() {
   
-  return <h1> hello word nlw</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20" >
+     <img src={logoImg} alt="" />
+
+     <h1 className="text-6xl text-white font-black mt-20">
+      Seu <span className='bg-nlw-gradient'>duo</span> está aqui
+     </h1>
+
+    </div>
+  )
 }
 
 export default App
