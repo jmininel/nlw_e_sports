1067863db9dfcbf592071d8e66fe2675c75d8b00
jmininel <ju.mininel@hotmail.com>
2022-09-14 19:00:20 -0300

    created prisma models

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,21 @@
 import express from 'express';
-
+import { PrismaClient } from "@prisma/client"
+ 
 const app = express()
+const prisma = new PrismaClient()
 
-app.get('/ads', (req, res) => {
+//listagem de games com contagem de anuncio
+app.get('/games', (req, res) => {
+    return res.json([]);
+});
+
+//Criação de novo anúncio
+app.post('/ads', (req, res) => {
+    return res.status(201).json([1,2]);
+});
+
+// Listagem de anúncios por game
+app.get('/games/:id/ads', (req, res) => {
     return res.json([
         {id: 1, name: 'Anuncio 1' },
         {id: 2, name: 'Anuncio 2' },
@@ -10,6 +23,11 @@ app.get('/ads', (req, res) => {
         {id: 4, name: 'Anuncio 3' },
         {id: 7, name: 'Anuncio 3' },
     ])
+})
+
+// Buscar Discord pelo ID do anúncio
+app.get('/ads/:id/discord', (req, res) => {
+    return res.json([])
 })
 
 app.listen(3333);
